0c67e1f72ae7ed3cfaeca6fd9c4b7d667d11d731
def <def@melexis.com>
2017-10-03 22:00:22 +0300

    Fix gauges to support non-zero min value

--- elm327reader/elm327reader.py
+++ elm327reader/elm327reader.py
@@ -240,7 +240,7 @@ class elm327reader(threading.Thread):
                     # some shit
                     for g in self.gauges:
                         if pid[3] == g[0]:
-                            num = int(min((pid[8] - g[1]) * 60 // g[2], 60))
+                            num = int(min((pid[8] - g[1]) * 60 // (g[2] - g[1]), 60))
                             print('    [' + '#' * num + ' ' * (60-num) + ']')
                 except:
                     pass
