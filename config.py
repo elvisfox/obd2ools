21aa725f2b8e0766a50e2db1d0b1244c036e4537
def <def@melexis.com>
2017-11-06 13:47:31 +0200

    Update some PIDs

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from pids.nissan import *
 from pids.parsed import *
 
 # use 'loop' to run with emulator
-port_name = 'loop'
-#port_name = 'COM10'
+#port_name = 'loop'
+port_name = 'COM10'
 baudrate = 115200
 
 fast_mode = False
@@ -18,28 +18,68 @@ init_list = [
 ]
 
 selected_pids = [
-	'Acc Pedal S2',
-	'Mass Air Flow',
-	'Inj Pulse B1',
-	'Inj Pulse B2',
-	'Fuel Base',
-	'RPM',
-	'A/F S1 (B1)',
-	'A/F S1 (B2)',
-	'Engine Load',
-	'H02S2 (B1)',
-	'H02S2 (B2)',
+#	'Acc Pedal S2',
+#	'Mass Air Flow',
+#	'MAF B1',
+#	'MAF B2',
+#	'Inj Pulse B1',
+#	'Inj Pulse B2',
+#	'Fuel Base',
+#	'RPM',
+	'Ign Adv',
+	'Ign Adv Adj',
+#	'TPS S1 (B1)',
+#	'A/F Ratio',
+#	'A/F Adj (B1)',
+#	'A/F Adj (B2)',
+#	'TPS S1 (B2)',
+#	'A/F S1 (B1)',
+#	'A/F S1 (B2)',
+	'STFT B1',
+	'STFT B2',
+#	'A/F S1 HTR(B1)',
+#	'A/F S1 HTR(B2)',
+#	'LTFT B1',
+#	'LTFT B2',
+#	'Car Speed',
+#	'INT/V TIM B1',
+#	'INT/V TIM B2',
+#	'EXH/V TIM B1',
+#	'EXH/V TIM B2',
+#	'VTC DTY IN B1',
+#	'VTC DTY IN B2',
+#	'VTC DTY EX B1',
+#	'VTC DTY EX B2',
+#	'Engine Load',
+#	'H02S2 (B1)',
+#	'H02S2 (B2)',
+#	'Current DTC',
+#	'Coolant T',
+#	'Intake Air T',
+#	'VVT Oil T',
 ]
 
 gauges = [
-	['RPM', 0, 4000],
+	['RPM', 0, 1500],
 	['Acc Pedal S2', 0.7, 4.2],
-	['TPS S1', 0, 5.0],
+	['TPS S1 (B1)', 0, 5.0],
+	['TPS S1 (B2)', 0, 5.0],
 	['A/F S1 (B1)', 0, 5.0],
 	['A/F S1 (B2)', 0, 5.0],
+	['MAF B1', 0, 5.0],
+	['MAF B2', 0, 5.0],
 	['Engine Load', 0, 100],
 	['H02S2 (B1)', 0, 1.0],
 	['H02S2 (B2)', 0, 1.0],
+	['STFT B1', -30, 30],
+	['STFT B2', -30, 30],
+	['LTFT B1', -30, 30],
+	['LTFT B2', -30, 30],
+	['Car Speed', 0, 120],
+	['Inj Pulse B1', 0, 10],
+	['Inj Pulse B2', 0, 10],
+	['A/F S1 HTR(B1)', 0, 100],
+	['A/F S1 HTR(B2)', 0, 100],
 ]
 
 # update interval
--- pids/nissan.py
+++ pids/nissan.py
@@ -10,24 +10,31 @@ def linear(data, gain, offset):
 nissan_pids = [
 	[0x806,	'0100',		'00000000',	'Sprt PIDs',	'deg',	'0.3f',	linear,	[0.0, 0]	],
 	[0x7E0,	'10C0',		'',			'Nissan Init',	'',		'0.0f',	linear,	[0.0, 0]	],
-	[0x7E0,	'221106',	'0000',		'Intake Air T',	'degC',	'0.3f',	linear,	[1.0, -50]	],		#  
-	[0x7E0,	'22110A',	'FF',		'Ign Adv',		'deg',	'0.3f',	linear,	[-1.0, 110] ],		# Ignition Timing Advance Angle, 8-bit unsigned, 0 = 110deg, 1 = -1.0deg
+	[0x7e0,	'221101',	'82',		'Coolant T',	'°С',	'0.0f',	linear,	[1.0, -50]	],
+	[0x7E0,	'221106',	'0000',		'Intake Air T',	'°C',	'0.0f',	linear,	[1.0, -50]	],		#  
+	[0x7E0,	'22110A',	'FF',		'Ign Adv',		'°',	'0.3f',	linear,	[-1.0, 110] ],		# Ignition Timing Advance Angle, 8-bit unsigned, 0 = 110deg, 1 = -1.0deg
 	[0x7E0,	'221110',	'01',		'EVAP Purge Duty', '%',	'0.3f',	linear,	[0.5, 0]	],		# EVAP Purge Duty Cycle, 1 = 0.5%
-	[0x7E0,	'221111',	'2D',		'Fuel T',		'degC',	'0.3f',	linear,	[1.0, -50]	],		# Fuel Temperature, offset=0x32 (50degC), 1 = 1degC
+	[0x7E0,	'221111',	'2D',		'Fuel T',		'°C',	'0.3f',	linear,	[1.0, -50]	],		# Fuel Temperature, offset=0x32 (50degC), 1 = 1degC
 	[0x7E0,	'221114',	'34',		'Fuel Level',	'%',	'0.0f',	linear,	[1.0, 0]	],		# ??? 
-	[0x7E0,	'221117',	'3A',		'Engine Load',	'%',	'0.0f',	linear,	[1.0/2.56, 0]	],	# 
+	[0x7E0,	'221117',	'3A',		'Engine Load',	'%',	'0.0f',	linear,	[1.0/2.56, 0]],		# 
 	[0x7E0,	'22111A',	'1C',		'H02S2 (B1)',	'V',	'0.2f',	linear,	[0.01, 0]	],		# 
 	[0x7E0,	'22111B',	'1C',		'H02S2 (B2)',	'V',	'0.2f',	linear,	[0.01, 0]	],		# 
-	[0x7E0,	'22111F',	'76',		'VVT Oil T',	'degC',	'0.3f',	linear,	[1.0, -50]	],		# VVT Oil Temperature, offset=0x32 (50degC), 1 = 1degC
-	[0x7E0,	'221123',	'64',		'STFT B1',		'%',	'0.0f',	linear,	[1.0, -100]	],		# A/F Ratio Adjustment (B1), offset=0x64, 1 = 1%
+	[0x7E0,	'22111F',	'76',		'VVT Oil T',	'°C',	'0.3f',	linear,	[1.0, -50]	],		# VVT Oil Temperature, offset=0x32 (50degC), 1 = 1degC
+	[0x7E0,	'221123',	'44',		'STFT B1',		'%',	'0.0f',	linear,	[1.0, -100]	],		# A/F Ratio Adjustment (B1), offset=0x64, 1 = 1%
 	[0x7E0,	'221124',	'84',		'STFT B2',		'%',	'0.0f',	linear,	[1.0, -100]	],		# A/F Ratio Adjustment (B2), offset=0x64, 1 = 1%
 	[0x7E0,	'221125',	'64',		'LTFT B1',		'%',	'0.0f',	linear,	[1.0, -100]	],		# A/F Ratio Adjustment (B1), offset=0x64, 1 = 1%
 	[0x7E0,	'221126',	'84',		'LTFT B2',		'%',	'0.0f',	linear,	[1.0, -100]	],		# A/F Ratio Adjustment (B2), offset=0x64, 1 = 1%
+	[0x7e0,	'22112D',	'00',		'Ign Adv Adj',	'°',	'0.0f',	linear,	[1.0,	0]	],
 	[0x7E0,	'221134',	'84',		'A/F Ratio',	'',		'0.0f',	linear,	[1.0,	0]	],		# 
-	[0x7E0,	'221135',	'7F',		'Cam Adv B1',	'deg',	'0.1f',	linear,	[0.5,	0]	],		# Camshaft Advance Angle (B1), 7-bit signed, 0 = 0deg, 1 = +0.5deg
-	[0x7E0,	'221137',	'04',		'Cam Adv B2',	'deg',	'0.1f',	linear,	[1.0, -128]	],		# Camshaft Advance Angle (B2), 7-bit signed, 0 = -128deg, 1 = +1.0deg
-	[0x7E0,	'221138',	'20',		'Cam Duty B1',	'%',	'0.3f',	linear,	[1.0/2.56, 0]	],	# Camshaft Valve Duty Cycle (B1), 1 = 1/2.56 %
-	[0x7E0,	'221139',	'20',		'Cam Duty B2',	'%',	'0.3f',	linear,	[1.0/2.56, 0]	],	# Camshaft Valve Duty Cycle (B2), 1 = 1/2.56 %
+	[0x7E0,	'221135',	'7F',		'INT/V TIM B1',	'°CA',	'0.1f',	linear,	[0.5, -64]	],		# Camshaft Advance Angle (B1), 7-bit signed, 0 = 0deg, 1 = +0.5deg
+	[0x7E0,	'221137',	'04',		'Cam Adv B2',	'deg',	'0.1f',	linear,	[0.5, -128]	],		# Camshaft Advance Angle (B2), 7-bit signed, 0 = -128deg, 1 = +1.0deg
+	[0x7E0,	'221138',	'20',		'Cam Duty B1',	'%',	'0.3f',	linear,	[1.0/2.56, 0]],		# Camshaft Valve Duty Cycle (B1), 1 = 1/2.56 %
+	[0x7E0,	'221139',	'20',		'Cam Duty B2',	'%',	'0.3f',	linear,	[1.0/2.56, 0]],		# Camshaft Valve Duty Cycle (B2), 1 = 1/2.56 %
+	[0x7e0,	'22114E',	'8D',		'A/F S1 HTR(B1)', '%',	'0.3f',	linear,	[1.0/2.56, 0]],
+	[0x7e0,	'22114F',	'A0',		'A/F S1 HTR(B2)', '%',	'0.3f',	linear,	[1.0/2.56, 0]],
+	[0x7e0,	'221163',	'81',		'INT/V TIM B2',	'°CA',	'0.3f',	linear,	[0.5, -64]	],
+	[0x7e0,	'221164',	'80',		'EXH/V TIM B1',	'°CA',	'0.3f',	linear,	[0.5, -64]	],
+	[0x7e0,	'221165',	'7F',		'EXH/V TIM B2',	'°CA',	'0.3f',	linear,	[0.5, -64]	],
  	[0x7E0,	'221201',	'0050',		'RPM',			'rpm',	'0.0f',	linear,	[12.5,	0]	],		# Engine RPM, 1 = 12.5 rpm
  	[0x7E0,	'221204',	'00E4',		'MAF B1',		'V',	'0.2f',	linear,	[0.005, 0]	],		# MAF Voltage (B1), 1 = 5mV
  	[0x7E0,	'221205',	'01D8',		'MAF B2',		'V',	'0.2f',	linear,	[0.005, 0]	],		# MAF Voltage (B2), 1 = 5mV
@@ -35,6 +42,7 @@ nissan_pids = [
  	[0x7E0,	'221207',	'0250',		'Inj Pulse B2',	'ms',	'0.2f',	linear,	[0.01,	0]	],		# Injector Pulse Width (B2), 100 = 1ms
  	[0x7E0,	'221208',	'2062',		'Fuel Base',	'ms',	'0.3f',	linear,	[0.001, 0]	],		# Fuel Base Pulse Width, 1000 = 1ms
  	[0x7E0,	'221209',	'01DA',		'Mass Air Flow','gm/s',	'0.2f',	linear,	[0.01,	0]	],		# Mass Air Flow, 100 = 1gm/s
+ 	[0x7e0,	'22120B',	'0000',		'Current DTC',	'',		'0.0f',	linear,	[1.0,	0]	],
  	[0x7E0,	'22120D',	'01DA',		'Acc Pedal S1',	'V',	'0.2f',	linear,	[0.005, 0]	],		# 
  	[0x7E0,	'22120E',	'01DA',		'Acc Pedal S2',	'V',	'0.2f',	linear,	[0.005, 0]	],		# 
  	[0x7E0,	'22120F',	'01DA',		'TPS S1 (B1)',	'V',	'0.2f',	linear,	[0.005, 0]	],		# 
@@ -42,8 +50,12 @@ nissan_pids = [
  	[0x7E0,	'22121A',	'01DA',		'Car Speed',	'km/h',	'0.1f',	linear,	[0.1,	0]	],		#
  	[0x7e0,	'221225',	'01B5',		'A/F S1 (B1)',	'V',	'0.2f',	linear,	[0.005, 0]	],		#
 	[0x7e0,	'221226',	'01AE',		'A/F S1 (B2)',	'V',	'0.2f',	linear,	[0.005, 0]	],		#
-	[0x7e0,	'221249',	'FFEB',		'A/F Adj (B1)',	'-',	'0.3f',	linear,	[0.0,	0]	],		#
-	[0x7e0,	'22124A',	'FFE7',		'A/F Adj (B2)',	'-',	'0.3f',	linear,	[0.0,	0]	],		#
+	[0x7e0,	'22122D',	'0000',		'VTC DTY IN B1','%',	'0.3f',	linear,	[3200/32768, 0]],
+	[0x7e0,	'22122E',	'0000',		'VTC DTY IN B2','%',	'0.3f',	linear,	[3200/32768, 0]],
+	[0x7e0,	'22122F',	'0000',		'VTC DTY EX B1','%',	'0.3f',	linear,	[3200/32768, 0]],
+	[0x7e0,	'221230',	'0000',		'VTC DTY EX B2','%',	'0.3f',	linear,	[3200/32768, 0]],
+	[0x7e0,	'221249',	'FFEB',		'A/F Adj (B1)',	'',		'0.2f',	linear,	[0.02,	0]	],		#
+	[0x7e0,	'22124A',	'FFE7',		'A/F Adj (B2)',	'',		'0.2f',	linear,	[0.02,	0]	],		#
 	[0x7e0,	'22124B',	'006E',		'TPS S1 (B2)',	'V',	'0.2f',	linear,	[0.005,	0]	],		#
 	[0x7e0,	'22124C',	'006D',		'TPS S2 (B2)',	'V',	'0.2f',	linear,	[0.005,	0]	],		#
 	[0x7E0,	'22122B',	'0064',		'Cruise Tgt',	'km/h',	'0.1f',	linear,	[0.1,	0]	],		# Cruise Control Target
