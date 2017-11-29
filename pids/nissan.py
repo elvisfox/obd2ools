a02ff1e450221804dce35dc5804098342796f903
def <def@melexis.com>
2017-11-29 20:27:32 +0200

    update PIDs etc

--- pids/nissan.py
+++ pids/nissan.py
@@ -28,8 +28,8 @@ nissan_pids = [
 	[0x7E0,	'221134',	'84',		'A/F Ratio',	'',		'0.0f',	linear,	[1.0,	0]	],		# 
 	[0x7E0,	'221135',	'7F',		'INT/V TIM B1',	'°CA',	'0.1f',	linear,	[0.5, -64]	],		# Camshaft Advance Angle (B1), 7-bit signed, 0 = 0deg, 1 = +0.5deg
 	[0x7E0,	'221137',	'04',		'Cam Adv B2',	'deg',	'0.1f',	linear,	[0.5, -128]	],		# Camshaft Advance Angle (B2), 7-bit signed, 0 = -128deg, 1 = +1.0deg
-	[0x7E0,	'221138',	'20',		'Cam Duty B1',	'%',	'0.3f',	linear,	[1.0/2.56, 0]],		# Camshaft Valve Duty Cycle (B1), 1 = 1/2.56 %
-	[0x7E0,	'221139',	'20',		'Cam Duty B2',	'%',	'0.3f',	linear,	[1.0/2.56, 0]],		# Camshaft Valve Duty Cycle (B2), 1 = 1/2.56 %
+	[0x7e0,	'221138',	'00',		'INT/V SOL(B1)', '%',	'0.3f',	linear,	[1.0/2.56, 0]],
+	[0x7e0,	'221139',	'00',		'INT/V SOL(B2)', '%',	'0.3f',	linear,	[1.0/2.56, 0]],
 	[0x7e0,	'22114E',	'8D',		'A/F S1 HTR(B1)', '%',	'0.3f',	linear,	[1.0/2.56, 0]],
 	[0x7e0,	'22114F',	'A0',		'A/F S1 HTR(B2)', '%',	'0.3f',	linear,	[1.0/2.56, 0]],
 	[0x7e0,	'221163',	'81',		'INT/V TIM B2',	'°CA',	'0.3f',	linear,	[0.5, -64]	],
@@ -42,7 +42,7 @@ nissan_pids = [
  	[0x7E0,	'221207',	'0250',		'Inj Pulse B2',	'ms',	'0.2f',	linear,	[0.01,	0]	],		# Injector Pulse Width (B2), 100 = 1ms
  	[0x7E0,	'221208',	'2062',		'Fuel Base',	'ms',	'0.3f',	linear,	[0.001, 0]	],		# Fuel Base Pulse Width, 1000 = 1ms
  	[0x7E0,	'221209',	'01DA',		'Mass Air Flow','gm/s',	'0.2f',	linear,	[0.01,	0]	],		# Mass Air Flow, 100 = 1gm/s
- 	[0x7e0,	'22120B',	'0000',		'Current DTC',	'',		'0.0f',	linear,	[1.0,	0]	],
+ 	[0x7e0,	'22120B',	'0000',		'Current DTC',	'',		'04X',	linear,	[1,		0]	],
  	[0x7E0,	'22120D',	'01DA',		'Acc Pedal S1',	'V',	'0.2f',	linear,	[0.005, 0]	],		# 
  	[0x7E0,	'22120E',	'01DA',		'Acc Pedal S2',	'V',	'0.2f',	linear,	[0.005, 0]	],		# 
  	[0x7E0,	'22120F',	'01DA',		'TPS S1 (B1)',	'V',	'0.2f',	linear,	[0.005, 0]	],		# 
@@ -50,8 +50,6 @@ nissan_pids = [
  	[0x7E0,	'22121A',	'01DA',		'Car Speed',	'km/h',	'0.1f',	linear,	[0.1,	0]	],		#
  	[0x7e0,	'221225',	'01B5',		'A/F S1 (B1)',	'V',	'0.2f',	linear,	[0.005, 0]	],		#
 	[0x7e0,	'221226',	'01AE',		'A/F S1 (B2)',	'V',	'0.2f',	linear,	[0.005, 0]	],		#
-	[0x7e0,	'22122D',	'0000',		'VTC DTY IN B1','%',	'0.3f',	linear,	[3200/32768, 0]],
-	[0x7e0,	'22122E',	'0000',		'VTC DTY IN B2','%',	'0.3f',	linear,	[3200/32768, 0]],
 	[0x7e0,	'22122F',	'0000',		'VTC DTY EX B1','%',	'0.3f',	linear,	[3200/32768, 0]],
 	[0x7e0,	'221230',	'0000',		'VTC DTY EX B2','%',	'0.3f',	linear,	[3200/32768, 0]],
 	[0x7e0,	'221249',	'FFEB',		'A/F Adj (B1)',	'',		'0.2f',	linear,	[0.02,	0]	],		#
@@ -59,4 +57,6 @@ nissan_pids = [
 	[0x7e0,	'22124B',	'006E',		'TPS S1 (B2)',	'V',	'0.2f',	linear,	[0.005,	0]	],		#
 	[0x7e0,	'22124C',	'006D',		'TPS S2 (B2)',	'V',	'0.2f',	linear,	[0.005,	0]	],		#
 	[0x7E0,	'22122B',	'0064',		'Cruise Tgt',	'km/h',	'0.1f',	linear,	[0.1,	0]	],		# Cruise Control Target
+	[0x7e0,	'221174',	'00',		'CO ADJUSTMENT', '%',	'0.3f',	linear,	[1.0/2.56,	0]],
+	[0x7e0,	'22130C',	'4100',		'SRT STATUS',	'',		'04X',	linear,	[1,		0]	],
 ]
